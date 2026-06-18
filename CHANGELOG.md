3847d15fcc3099ca05ab0e12acb80976578ff8e7
aholmes@mitre.org <14923551+holmesie@users.noreply.github.com>
2026-06-18 12:25:47 -0400

    Release v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.1.1
+
+* Set US Core Test Kit dependency to '~> 1.1', '>= 1.1.3'
+
 # 0.1.0
- 
+
 * Initial public release.
--- lib/us_quality_core_test_kit/version.rb
+++ lib/us_quality_core_test_kit/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 module USQualityCoreTestKit
-  VERSION = '0.1.0'
+  VERSION = '0.1.1'
   LAST_UPDATED = '2026-06-18'
 end
